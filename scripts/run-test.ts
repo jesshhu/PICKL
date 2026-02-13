b442f6aab718be8e02354fc4b3a67446116a79a7
Jessu M <63313429+jesshhu@users.noreply.github.com>
2026-02-13 11:16:17 +0800

    Revert "Feat/login and product code"

--- scripts/run-test.ts
+++ scripts/run-test.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { execSync } from 'child_process'
+import { exec } from 'child_process'
 import 'dotenv/config'
 
 // Parse command line arguments
@@ -30,4 +30,11 @@ const nodeOptions = existingNodeOptions
 
 const command = `cross-env NODE_OPTIONS="${nodeOptions}" cucumber-js --config cucumber.js --import 'test/support/**/*.ts' --import 'test/steps/**/*.ts' --format ./test/support/verbose-formatter.ts --format json:test-results/cucumber-report.json ${tagsOption} ${featurePath}`
 
-execSync(command, { stdio: 'inherit' })
+const child = exec(command, error => {
+  if (error) {
+    process.exit(error.code ?? 1)
+  }
+})
+
+child.stdout?.pipe(process.stdout)
+child.stderr?.pipe(process.stderr)
